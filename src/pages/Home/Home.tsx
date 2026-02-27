6857d728083ce8696bbf0da9a09817a581384003
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2026-02-27 02:40:50 -0500

    🧠 Update Home to include all sections

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,16 @@
+import EmailCaptureSection from "../../sections/EmailCaptureSection";
 import FooterSection from "../../sections/Footer";
 import HatVideoCardSection from "../../sections/HatVideoCardSection";
 import HeroSection from "../../sections/HeroSection";
 import LegacyStatsSection from "../../sections/LegacyStatsSection";
+import SealSection from "../../sections/SealSection";
 
 export default function Home() {
   return (
     <main className="bg-white text-zinc-900">
       <HeroSection />
+      <EmailCaptureSection />
+      <SealSection />
       <LegacyStatsSection />
       <HatVideoCardSection />
       <FooterSection />
