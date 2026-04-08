c6a9c31f6b84193966f50f818e7f2c0926caad9b
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2026-04-08 02:48:43 -0400

    add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"; // 👈 ADD THIS
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -8,6 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics /> {/* 👈 ADD THIS */}
     </BrowserRouter>
   </React.StrictMode>,
 );
